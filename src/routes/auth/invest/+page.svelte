12ef9a9e2541d4c8330f3bfc5db1eeb3a0e908b7
Isadora Paz <85000060+Ashlc@users.noreply.github.com>
2024-10-23 12:59:13 -0300

    :construction: feat: Investment page

--- src/routes/auth/invest/+page.svelte
+++ src/routes/auth/invest/+page.svelte
@@ -1,10 +1,57 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { Badge } from 'flowbite-svelte';
 	import Layout from '../../../components/Layout.svelte';
+	let balance = 34245.23;
+
+	interface Rentability {
+		value: number;
+		style: 'positive' | 'negative';
+	}
+
+	const rentability: Rentability = {
+		value: 135.23,
+		style: 'positive'
+	};
+
+	let formattedBalance = balance.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+	let formattedRentability = rentability.value.toLocaleString('pt-BR', {
+		style: 'currency',
+		currency: 'BRL'
+	});
+
+	const styles = {
+		positive: {
+			color: 'text-green-500',
+			icon: 'material-symbols:arrow-upward'
+		},
+		negative: {
+			color: 'text-red-500',
+			icon: 'material-symbols:arrow-downward'
+		}
+	};
 </script>
 
 <Layout root="Início" startIcon="ic:outline-explore" breadcrumbItems={['Investimentos']}>
 	<div class="flex flex-row gap-6">
-		<div class="flex basis-3/4 flex-col gap-12"></div>
+		<div class="flex basis-3/4 flex-col gap-12">
+			<div class="flex flex-col gap-3">
+				<div class="flex flex-row items-center gap-4 text-primary-100">
+					<Icon icon="material-symbols:trending-up" height="16" />
+					<p class="font-semibold text-primary-200">Carteira de investimentos</p>
+				</div>
+				<div class="flex flex-row items-center gap-4">
+					<p class="w-fit text-4xl font-bold text-primary-500">{formattedBalance}</p>
+					<Badge large class={`gap-1 border bg-white`} rounded>
+						<div class={`flex flex-row items-center gap-[2px] ${styles[rentability.style].color}`}>
+							<p class="font-semibold">{formattedRentability}</p>
+							<Icon icon={styles[rentability.style].icon} class="mb-[1px]" />
+						</div>
+						<span>de rendimento</span>
+					</Badge>
+				</div>
+			</div>
+		</div>
 		<div class="flex basis-1/4 flex-col"></div>
 	</div></Layout
 >
