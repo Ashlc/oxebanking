51c9a7e020ff368cbd39d645b091498b73e8de49
Isadora Paz <85000060+Ashlc@users.noreply.github.com>
2024-10-24 07:58:10 -0300

    :sparkles: feat: Working graph

--- src/routes/auth/invest/Graph.svelte
+++ src/routes/auth/invest/Graph.svelte
@@ -36,7 +36,7 @@
 
 	// Function to shuffle an array
 	function shuffleArray(array: number[]): number[] {
-		for (let i = 9 - 1; i > 0; i--) {
+		for (let i = 10 - 1; i > 0; i--) {
 			const j = Math.floor(Math.random() * (i + 1));
 			[array[i], array[j]] = [array[j], array[i]];
 		}
@@ -44,13 +44,24 @@
 	}
 
 	onMount(() => {
-		var chart = echarts.init(chartContainer, null, {renderer: 'svg', width: '800'});
+		var chart = echarts.init(chartContainer, null, { renderer: 'svg' });
 		const originalData = [25, 14, 23, 35, 10, 8, 11, 29, 14, 15, 16, 18, 20, 22, 24, 26, 28];
 		const shuffledData = shuffleArray(originalData); // Shuffle the data
 
 		chart.setOption({
 			xAxis: {
-				data: ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00']
+				data: [
+					'09:00',
+					'10:00',
+					'11:00',
+					'12:00',
+					'13:00',
+					'14:00',
+					'15:00',
+					'16:00',
+					'17:00',
+					'18:00'
+				]
 			},
 			color: ['#78bc61', '#a8e174', '#d3e49a', '#d0648a', '#f58db2', '#f2b3c9'],
 			yAxis: {},
@@ -64,21 +75,26 @@
 						opacity: 0.5
 					}
 				}
-			]
+			],
+			grid: {
+				top: 30,
+				bottom: 30,
+				left: '2%',
+				right: 0
+			}
 		});
 	});
-
 </script>
 
-<div class="flex flex-col p-4 border rounded-lg w-full">
-  <div class="flex flex-row gap-2">
-    <p class="text-lg font-semibold">Visão geral</p>
-    <Badge large class={`gap-1 border bg-white`} rounded>
+<div class="flex w-full flex-col rounded-lg border p-4">
+	<div class="flex flex-row gap-2">
+		<p class="text-lg font-semibold">Visão geral</p>
+		<Badge large class={`gap-1 border bg-white`} rounded>
 			<div class={`flex flex-row items-center gap-[2px] ${styles[rentability.style].color}`}>
-			  <p class="font-semibold">{formattedRentability}</p>
-			  <Icon icon={styles[rentability.style].icon} class="mb-[1px]" />
-		  </div>
+				<p class="font-semibold">{formattedRentability}</p>
+				<Icon icon={styles[rentability.style].icon} class="mb-[1px]" />
+			</div>
 		</Badge>
-  </div>
-  <div bind:this={chartContainer} class="grow h-[400px]"></div>
+	</div>
+	<div bind:this={chartContainer} class="h-[400px] grow"></div>
 </div>
