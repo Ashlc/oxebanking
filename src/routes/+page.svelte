e5878e1df51d15978645467689f3b2f8b0c14e7e
Isadora Paz <85000060+Ashlc@users.noreply.github.com>
2024-10-30 09:51:04 -0300

    :sparkles: feat: Forgot password view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,39 +16,69 @@
 		const formattedCpf = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
 		(event.target as HTMLInputElement).value = formattedCpf;
 	}
+
+	let forgotPassword = false;
 </script>
 
 <div class="h-screen w-screen p-4">
 	<div class="flex h-full flex-row gap-4">
-		<div class="flex h-full basis-1/3 flex-col items-center justify-center gap-10">
-			<OxebankingLogo width={250} height={80} />
-			<form
-				class="flex w-1/2 flex-col gap-4"
-				id="login-form"
-				bind:this={loginForm}
-				on:submit={handleSubmit}
-			>
-				<div>
-					<Label>CPF</Label>
-					<Input type="text" required minlength={11} maxlength={11} on:blur={validateCpf} />
-				</div>
-				<div>
-					<Label>Senha</Label>
-					<Input type="password" required />
-				</div>
-				<a href="/forgot-password" class="text-right text-sm text-secondary-100"
-					>Esqueci minha senha</a
+		{#if !forgotPassword}
+			<div class="flex h-full basis-1/3 flex-col items-center justify-center gap-10">
+				<OxebankingLogo width={250} height={80} />
+				<form
+					class="flex w-1/2 flex-col gap-4"
+					id="login-form"
+					bind:this={loginForm}
+					on:submit={handleSubmit}
 				>
-				<Button class="mt-4 w-full" type="submit" form="login-form">ENTRAR</Button>
-			</form>
-			<div class="flex w-1/2 flex-col items-center gap-2">
-				<p>Ôxe, tu não tem conta?</p>
-				<Button class="w-full border-primary-500 text-primary-700" color="light">
-					ABRA UMA CONTA OXE
-				</Button>
+					<div>
+						<Label>CPF</Label>
+						<Input type="text" required minlength={11} maxlength={11} on:blur={validateCpf} />
+					</div>
+					<div>
+						<Label>Senha</Label>
+						<Input type="password" required />
+					</div>
+					<button
+						on:click={() => (forgotPassword = true)}
+						class="text-right text-sm text-secondary-100 transition-all hover:underline"
+						>Esqueci minha senha</button
+					>
+					<Button class="mt-4 w-full" type="submit" form="login-form">ENTRAR</Button>
+				</form>
+				<div class="flex w-1/2 flex-col items-center gap-2">
+					<p>Ôxe, tu não tem conta?</p>
+					<Button class="w-full border-primary-500 text-primary-700" color="light">
+						ABRA UMA CONTA OXE
+					</Button>
+				</div>
 			</div>
-		</div>
-		<div class="m relative h-full basis-2/3 overflow-clip rounded-2xl bg-secondary-200">
+		{:else}
+			<div class="flex h-full basis-1/3 flex-col items-center justify-center gap-10">
+				<div class="flex w-1/2 flex-col gap-4">
+					<div class="-ml-3">
+						<OxebankingLogo height={80} />
+					</div>
+					<p>Para recuperar sua senha, informe seu CPF e clique em recuperar senha.</p>
+				</div>
+				<form
+					class="flex w-1/2 flex-col gap-4"
+					id="login-form"
+					bind:this={loginForm}
+					on:submit={handleSubmit}
+				>
+					<div>
+						<Label>CPF</Label>
+						<Input type="text" required minlength={11} maxlength={11} on:blur={validateCpf} />
+					</div>
+					<Button class="mt-4 w-full" type="submit" form="login-form">RECUPERAR SENHA</Button>
+					<Button color="light" class="border-none" on:click={() => (forgotPassword = false)}
+						>VOLTAR</Button
+					>
+				</form>
+			</div>
+		{/if}
+		<div class="relative h-full basis-2/3 overflow-clip rounded-2xl bg-secondary-200">
 			<h2 class="absolute bottom-16 z-10 w-full text-center text-4xl font-bold text-white">
 				Do nordeste para o Brasil. <br />
 				Há mais de 5 anos.
